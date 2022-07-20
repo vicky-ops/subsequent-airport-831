171bbd3430c73d0e067f61e40b250b72c401bfdc
PRATEEK SONI <nmprateek9425@gmail.com>
2022-07-20 22:19:35 +0530

    navbar + nextpage

---
+++ project.js
@@ -0,0 +1,17 @@
+
+
+let input = document.getElementById("searchtab")
+
+input.addEventListener("keyup",(el)=>{
+if(el.keyCode===13){
+    // console.log(el.target.value)
+    window.location.href="page2.html"
+
+
+
+}
+
+})
+
+
+
