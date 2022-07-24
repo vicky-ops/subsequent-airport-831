a2157f3824a2f9c5fb775c919dd4a2081cb17583
vicky-d <workmailvicky101@gmail.com>
2022-07-24 05:54:38 +0530

    --added necessary changes

--- travellerDetails.js
+++
@@ -1,6 +0,0 @@
-import navBar_payment from "./navBar.js";
-
-// console.log(navBar_payment);
-document.querySelector("#navigation").innerHTML = navBar_payment();
-
-let data = JSON.parse(localStorage.getItem("users")) || [];
